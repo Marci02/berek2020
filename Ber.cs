333b0a6d28e862ded2f787b870571a15d96f73a1
Marci02 <nagymarci.0218@gmail.com>
2024-11-19 09:26:50 +0100

    Add project files.

---
+++ Ber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace berek2020
+{
+    internal class Ber
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Fizetes { get; set; }
+
+        public Ber(string sor)
+        {
+            var v = sor.Split(';');
+            Nev = v[0];
+            Nem = v[1] == "F";
+            Reszleg = v[2];
+            Belepes = int.Parse(v[3]);
+            Fizetes = int.Parse(v[4]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using berek2020;
+using System.Text;
+
+
+//9:05 - 9:24 19 perc alatt lett kész
+
+List<Ber> berek = new();
+using(StreamReader sr = new StreamReader(@"..\..\..\src\berek2020.txt", Encoding.UTF8))
+{
+    while (!sr.EndOfStream)
+    {
+        berek.Add(new Ber(sr.ReadLine()));
+    }
+}
+
+Console.WriteLine($"3. feladat: A forrásállományban {berek.Count} db dolgozó adatait tároljuk.");
+
+
+
+var f4 = berek.Average(x => x.Fizetes);
+
+Console.WriteLine($"4. feladat: Az éves átlag fizetés 2020-ban {f4} Ft");
+
+Console.Write("Adjon meg egy részleget: ");
+var beker = Console.ReadLine();
+var f6 = berek.Where(x => x.Reszleg == beker);
+
+if(f6.Count() == 0) Console.WriteLine("6. feladat: A megadott részleg nem létezik a cégnél!");
+if(f6.Count() > 0) Console.WriteLine($"6. feladat: A megadott részleg legmagasabb fizetése: {f6.Max(x => x.Fizetes)} Ft");
+
+
+var f7 = berek.GroupBy(x => x.Reszleg);
+foreach (var item in f7)
+{
+    Console.WriteLine($"{item.Key} - {item.Count()} fő");
+}
+
